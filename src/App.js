de88d800eac698907d1c2acd8fb6b72e1be26d31
frsiam <siam.frs@gmail.com>
2022-04-20 23:56:16 +0600

    install bootstrap then add a simple form to get input from user then take input value by event.target method

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import { useEffect, useState } from 'react';
 function App() {
   const [users, setUsers] = useState([])
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch('http://localhost:5000/users')
-    .then(res => res.json())
-    .then(data => setUsers(data))
-  },[])
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+  const handleAddUser = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = {name, email};
+    console.log(name, email)
+
+    // post data to server 
+  }
+
   return (
     <div className="App">
       <h1>My own data: {users.length}</h1>
-      <form>
-        <input type="text" name="name" id="" placeholder='Name' />
-        <input type="email" name="email" id="" placeholder='Email' />
-        <input type="submit" value="Add User" />
+      <form onSubmit={handleAddUser} className='w-50 mx-auto my-5 border p-3 border-primary'>
+        <div className="mb-3">
+          <input name='name' type="text" className="form-control fs-5 bg-info" id="exampleInputPassword1" placeholder='Name' />
+        </div>
+        <div className="mb-3">
+          <input name='email' type="email" className="form-control bg-warning fs-5" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder='Email' />
+        </div>
+        <button type="submit" className="btn btn-primary">Add User</button>
       </form>
       <ul>
         {
